752161c7d5e98b893b0b8264bb508f2742383832
Romano Schoonheim <romano@romanoschoonheim.nl>
2022-05-01 13:55:09 +0200

    master: Performance test..

---
+++ src/AdvancedFactories.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Rschoonheim\LaravelAdvancedFactories;
+
+/**
+ * class AdvancedFactories.
+ *
+ * @package Rschoonheim\LaravelAdvancedFactories;
+ */
+class AdvancedFactories
+{
+
+}
