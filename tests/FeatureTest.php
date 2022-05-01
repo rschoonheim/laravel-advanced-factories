37d4207db903cbefa7ed6228a44c6e158ec55faf
Romano Schoonheim <romano@romanoschoonheim.nl>
2022-05-01 13:45:29 +0200

    master: phpunit setup.

---
+++ tests/FeatureTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Rschoonheim\LaravelAdvancedFactories\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+/**
+ * class FeatureTest.
+ *
+ * @package Rschoonheim\LaravelAdvancedFactories\Tests;
+ */
+abstract class FeatureTest extends TestCase
+{
+
+}
